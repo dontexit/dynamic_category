abd157b70f27f7860374d9a3a0a9c139ff032178
Edwin Hamal Thakuri <edwinhamal@gmail.com>
2022-03-29 01:47:20 +0545

    Update subcat.js

--- subcat.js
+++ subcat.js
@@ -1,68 +1,79 @@
+
+    const category = document.querySelector('#id_category')
+    const subcategory = document.querySelector('#id_subcategory')
+    category.addEventListener("change",catFunc)
     const sub = [
-    ["Cloth",],
-    ["Vechile"],
-    [""],
-    [""],
-    [""],
-    [""],
-    [""],
-    [""],
-    [""],
-    ["Mobile"],
-    [""],
-    [""],
-    [""],
-    [""],
-    ["VideoGame"],
-    [""],
-    [""],
-]
+    {"CT": ["Cloth"],},
+    {"AM":["Vechile"]},
+    {"EG":[""]},
+    {"BH":[""]},
+    {"BI":[""]},
+    {"CP":[""]},
+    {"EH":[""]},
+    {"GS":[""]},
+    {"FA":[""]},
+    {"MA":["Mobile"]},
+    {"JB":[""]},
+    {"MI":[""]},
+    {"PT":[""]},
+    {"SV":[""]},
+    {"TY":["VideoGame"]},
+    {"VG":[""]},
+    {"OT":[""]},
+    ]
+    function subcategory_reset(){
+        while (subcategory.childNodes.length > 2) {
+        subcategory.removeChild(subcategory.lastChild);
+    }
+    }
+    subcategory_reset()
 
-  const category = document.querySelector('#id_category')
-  category.addEventListener("change", myScript)
-  var subcat = document.querySelector('#id_subcategory')  
-  subcat.removeChild(subcat.lastChild);
-
-  function myScript(){ 
-    while (subcat.childNodes.length > 2) {
-    subcat.removeChild(subcat.lastChild);
-  }
-  if (category){
-      const sub_len = sub[category.value-1].length
-      console.log(sub_len)
-      if(sub_len > 1){
-        for(let i=0; i < sub_len; i++){
-          var opt = document.createElement('option')
-          opt.value = i
-          opt.innerHTML = sub[category.value-1][i]
-          console.log("more than",opt.value)
-          console.log(opt.innerHTML)
-          subcat.appendChild(opt)
-          subcat.addEventListener("change", changeForm)
+    function get_sublist(subcategory_list){
+        // console.log(subcategory_list)
+        if (category.value == ""){
+            console.log("empty")
+            return
         }
-      }
-      else{
-        var opt = document.createElement('option')
-        opt.value = "1"
-        opt.innerHTML = sub[category.value-1][0]
-        console.log(opt.value)
-        console.log(opt.innerHTML)
-        subcat.appendChild(opt)
-        subcat.addEventListener("change", changeForm)
-      }
+        for (let i=0; i < subcategory_list.length; i++){
+            if(category.value in subcategory_list[i]){
+                console.log(subcategory_list[i])
+                   // subcatergory list with same key as selceted category found
+                   return subcategory_list[i][category.value] 
+            }
+        }
     }
-  }
 
-  function changeForm(){
-    if (subcat){
-    fetch('api/form/'+category.value+'/'+subcat.value+'/')
-        .then(response =>{
-            console.log(response.json())
-            return response.json
-        })
-        .then(function (data) {
-        const formContainer = document.querySelector(".form-container");
-        formContainer.innerHTML = data.form
-    })
+    function displaysub(subcategory_list){
+            for(let i=0; i < subcategory_list.length; i++){
+                console.log(subcategory_list[i])
+                var opt = document.createElement('option')
+                opt.value = subcategory_list[i]
+                opt.innerHTML = subcategory_list[i]
+                console.log("more than",opt.value)
+                console.log(opt.innerHTML)
+                subcategory.appendChild(opt)
+            }
+        }
+
+    function catFunc(){
+        subcategory_reset()
+        if (category.value != ""){
+            displaysub(get_sublist(sub))
+
+        }
+            
     }
-  }
+    
+    function log_categorychange(){
+        if (category.value != ""){
+            
+        }
+    }
+    subcategory.addEventListener("change",get_sub)
+
+    function get_sub(){
+        if(subcategory){
+            console.log(subcategory.value)
+        }
+    }
+    get_sub()
