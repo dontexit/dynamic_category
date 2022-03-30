d0185e0e6ef7477a9d6a3d7462a2f71eca4afca0
Edwin Hamal Thakuri <edwinhamal@gmail.com>
2022-03-31 00:08:39 +0545

    Update subcat.js

--- subcat.js
+++ subcat.js
@@ -77,3 +77,77 @@
         }
     }
     get_sub()
+
+#Index
+
+   <ul class="category_list" style="position: absolute; top:0; display: none; flex-direction: column; height: 100vh; background-color: azure; gap: 0.5rem;  width: 300px;">
+        <div class="category-menu" style="display: flex; gap: 9rem;">
+            <p id="category_title">All Categories</p>
+            <img id="x" onclick=hidecat() src="{% static 'account/icons/x.svg' %}" alt=""></li>
+        </div>
+        {% for category in categories %}
+    
+    <li>
+        <a id="{{ category.0 }}" href="#">{{category.1}}</a>
+        <div class="{{ category.0  }}"></div>
+    </li>
+{% endfor %}
+</ul>
+</div>
+<script>
+
+    cl = document.querySelector('.category_list')
+    cl.addEventListener("click",cat_changed)
+    
+    function get_sublist(subcategory_list,value){
+        // console.log(subcategory_list)
+        for (let i=0; i < subcategory_list.length; i++){
+            if(value in subcategory_list[i]){
+                console.log(subcategory_list[i])
+                   // subcatergory list with same key as selceted category found
+                   return subcategory_list[i][value] 
+            }
+        }
+    }
+  
+     
+    function cat_changed(evt){
+        if(get_sublist(sub,evt.target.id)){
+            scat = document.querySelector('.'+evt.target.id)
+            console.log(scat)
+            if(scat.childNodes.length > 0){
+                console.log(scat.childNodes.length)
+                document.querySelector('.'+evt.target.id).style.display='none'
+    //            while (scat.childNodes.length > 0) {
+    //     scat.removeChild(scat.lastChild);
+    //     subcat.classList.add('sub')
+    // }
+            }else{
+            console.log(get_sublist(sub,evt.target.id))
+            showsub(evt.target.id,get_sublist(sub,evt.target.id))
+        }
+        console.log(typeof(evt.target.id))
+    }
+}
+
+    
+
+    function showsub(sid,subcategory_list){
+            for(let i=0; i < subcategory_list.length; i++){
+                console.log("diplaying",subcategory_list[i])
+                subcat = document.querySelector("."+sid)
+                subcat.classList.add('sub')
+                var a = document.createElement('a')
+                a.innerHTML = subcategory_list[i]
+                a.href = "#"
+                console.log(subcat)
+                subcat.appendChild(a)
+            }
+        }
+    
+    function hidecat(){
+        cl.style.display = "none"
+    }
+    function barClicked(){
+        cl.style.display = "flex"
+    }
